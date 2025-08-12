5022bcb74532361fd7cfd92952e3b4ff50fc8142
hzeng412 <79126211+hzeng412@users.noreply.github.com>
2025-08-12 16:09:03 -0700

    updated campathon page

--- docs/assets/_astro_content.CL-nA4ps.js
+++ docs/assets/_astro_content.CL-nA4ps.js
@@ -255,7 +255,7 @@ const CONTENT_LAYER_IMAGE_REGEX = /__ASTRO_IMAGE_="([^"]+)"/g;
 async function updateImageReferencesInBody(html, fileName) {
   const { default: imageAssetMap } = await import('./content-assets.DleWbedO.js');
   const imageObjects = /* @__PURE__ */ new Map();
-  const { getImage } = await import('./_astro_assets.ClGsQ7O4.js').then(n => n._);
+  const { getImage } = await import('./_astro_assets.CJz4meMq.js').then(n => n._);
   for (const [_full, imagePath] of html.matchAll(CONTENT_LAYER_IMAGE_REGEX)) {
     try {
       const decodedImagePath = JSON.parse(imagePath.replaceAll("&#x22;", '"'));
