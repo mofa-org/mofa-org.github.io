4c847b63e7f04a1e7030d1cf6cbcb1042b0f3056
bh3gei <scholar.liyao@gmail.com>
2025-10-22 20:48:02 +0900

    feat: Add MoFA Site links and hide GitHub Stats tab
    - Add username.mofa.ai link for every contributor in Hall of Fame
    - Show MoFA Site link in both hover cards and modal
    - Hide GitHub Repository Stats tab (filtered by type: repo_stats)
    - Add project information display with prize badges
    - Support for project URL and project name fields
    - Green 'MoFA Site' button with globe icon

--- docs/assets/sharp.D9nYzAgW.js
+++ docs/assets/sharp.D9nYzAgW.js
@@ -1,5 +1,5 @@
 import { A as AstroError, z as MissingSharp } from './astro/server.C5Q1HgHb.js';
-import { b as baseService, p as parseQuality } from './_astro_assets.c5fD1SO6.js';
+import { b as baseService, p as parseQuality } from './_astro_assets.BVsOc_36.js';
 
 let sharp;
 const qualityTable = {
