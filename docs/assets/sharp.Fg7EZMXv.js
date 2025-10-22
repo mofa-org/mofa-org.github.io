aa51fea51cddaaf679232ba2855cb3b093cef8ad
bh3gei <scholar.liyao@gmail.com>
2025-10-22 20:28:05 +0900

    refactor: Separate Hackathons into standalone navigation section
    - Create dedicated 'Hackathons' dropdown in main navigation
    - Add direct link to Hall of Fame with anchor jump
    - Move hackathon links from Community to Hackathons section
    - Update both desktop and mobile navigation layouts
    - Improve discoverability of competition and winner information

--- docs/assets/sharp.Fg7EZMXv.js
+++ docs/assets/sharp.Fg7EZMXv.js
@@ -1,5 +1,5 @@
 import { A as AstroError, z as MissingSharp } from './astro/server.C5Q1HgHb.js';
-import { b as baseService, p as parseQuality } from './_astro_assets.CI-tAkUJ.js';
+import { b as baseService, p as parseQuality } from './_astro_assets.c5fD1SO6.js';
 
 let sharp;
 const qualityTable = {
