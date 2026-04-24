35afe93954767b8e421312ad67176d85a318435a
bh3gei <scholar.liyao@gmail.com>
2026-04-24 11:18:53 +0900

    Smooth video loop: CSS transition fade out 1.5s before end, fade in 0.8s on restart

--- docs/assets/sharp.DRlaTLxZ.js
+++ docs/assets/sharp.DRlaTLxZ.js
@@ -1,5 +1,5 @@
 import { A as AstroError, z as MissingSharp } from './astro/server.K-yHHyjY.js';
-import { b as baseService, p as parseQuality } from './_astro_assets.BdM7hKzr.js';
+import { b as baseService, p as parseQuality } from './_astro_assets.CSpXPqqh.js';
 
 let sharp;
 const qualityTable = {
